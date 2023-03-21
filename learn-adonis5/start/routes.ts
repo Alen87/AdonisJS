ea1d32ec53049f7dfc40022634634abf0eb2cd63
Alen Boric <boricalen@live.com>
2023-03-21 17:37:18 +0100

    dynamic routing

--- learn-adonis5/start/routes.ts
+++ learn-adonis5/start/routes.ts
@@ -20,6 +20,18 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
+Route.where('id',{
+  match:/^[0-9]+$/,
+  cast:(id)=>Number(id)
+})
+
+
+Route.get('/img/:userId/*',async({params})=>{
+  return params
+})
+
+
+
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
 })
@@ -30,11 +42,36 @@ return 'working'
 })
 
 
-Route.get('/posts',()=>{})      // listing posts
-Route.get('/posts/1',async ()=>{})  // get single post
-Route.post('/posts',async ()=>{})  // creating a post
-Route.put('/posts/1',async ()=>{})  // updating a post
-Route.delete('/posts/1',async()=>{})  //deleting a post
+
+
+
+Route.get('/posts',()=>{
+  return 'listing posts'
+}) 
+
+Route.get('/posts/:id',async ({params})=>{
+  return `get single post with an id of ${typeof params.id}`
+})
+
+Route.post('/posts',async ()=>{
+  return 'creating a post'
+}) 
+
+Route.put('/posts/:id',async ({params})=>{
+  return `updating a post with an id of ${params.id}`
+}) 
+
+Route.delete('/posts/:id',async({params})=>{
+  return `deleting a post with an id of ${params.id}`
+}) 
+
+
+Route.get('/posts/topics/:topic?', async ({params})=>{
+  return `topic is ${params.topic}`
+}).where('topics' , Route.matchers.slug())
+
+
+
 
 // Route.any('/posts', async()=>{})
 
