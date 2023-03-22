ba2454212caf24858ce85a3591e5f5a674fb21b4
Alen Boric <boricalen@live.com>
2023-03-22 02:06:54 +0100

    Organizing Routes

--- learn-adonis5/start/routes.ts
+++ learn-adonis5/start/routes.ts
@@ -20,6 +20,8 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
+import 'App/Modules/Posts/routes'
+
 Route.where('id',{
   match:/^[0-9]+$/,
   cast:(id)=>Number(id)
@@ -43,37 +45,6 @@ return 'working'
 
 
 
-
-
-Route.get('/posts',()=>{
-  return 'listing posts'
-}) 
-
-Route.get('/posts/:id',async ({params})=>{
-  return `get single post with an id of ${typeof params.id}`
-})
-
-Route.post('/posts',async ()=>{
-  return 'creating a post'
-}) 
-
-Route.put('/posts/:id',async ({params})=>{
-  return `updating a post with an id of ${params.id}`
-}) 
-
-Route.delete('/posts/:id',async({params})=>{
-  return `deleting a post with an id of ${params.id}`
-}) 
-
-
-Route.get('/posts/topics/:topic?', async ({params})=>{
-  return `topic is ${params.topic}`
-}).where('topics' , Route.matchers.slug())
-
-
-
-
-// Route.any('/posts', async()=>{})
 
 Route.on('/testing').redirectToPath('https://duckduckgo.com')
 
