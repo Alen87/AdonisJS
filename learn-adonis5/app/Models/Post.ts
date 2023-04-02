611bb553546780962b73a7ad3721ffe46bd4429d
Alen Boric <boricalen@live.com>
2023-04-02 22:48:14 +0200

    Database Migration to Lucid Models

--- learn-adonis5/app/Models/Post.ts
+++ learn-adonis5/app/Models/Post.ts
@@ -1,9 +1,29 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
+import {
+  attachment,
+  AttachmentContract
+} from '@ioc:Adonis/Addons/AttachmentLite'
+
+
 export default class Post extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+   public  id: number
+  @column()
+   public userId:number
+  @column()
+  public stateId : number
+  @column()
+  public title :string
+  @column()
+   public slug:string
+  @column()
+  public description :string | null
+  @column()
+  public body :string | null
+  @attachment()
+  public thumbnail :AttachmentContract | null
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
--- learn-adonis5/app/Models/Profile.ts
+++ learn-adonis5/app/Models/Profile.ts
@@ -1,9 +1,24 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import {
+  attachment,
+  AttachmentContract
+} from '@ioc:Adonis/Addons/AttachmentLite'
 
 export default class Profile extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public userId: number
+  
+  @column()
+  public biography: string | null
+
+  @attachment()
+  public avatar:AttachmentContract | null
+ 
+
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
